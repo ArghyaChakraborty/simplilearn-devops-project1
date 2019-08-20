79498f2cf96c4a08dd0653fbce3893b6c0d0a66f
501954963 <arghya.chakraborty@ge.com>
2019-08-20 15:34:44 -0500

    Adjusted pipeline build trigger definition

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,11 +1,11 @@
 pipelineJob('SimplilearnProject1PipelineJob') {
 description('This pipeline job is for Simplilearn Project 1')
-    definition {
+GenericTrigger {
+	token("SimplilearnProject1PipelineJob")
+}
+definition {
         cps {
 			sandbox()
-			GenericTrigger {
-				token("SimplilearnProject1PipelineJob")
-			}
             script(readFileFromWorkspace('pipeline/java_maven_build_with_docker_deployment.groovy'))
         }
     }
