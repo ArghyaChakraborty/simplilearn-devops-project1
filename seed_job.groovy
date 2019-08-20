d7e4b6933ac4dc1051323ff690092c98d5a88726
501954963 <arghya.chakraborty@ge.com>
2019-08-20 15:30:24 -0500

    Added build trigger to pipeline definition

--- seed_job.groovy
+++ seed_job.groovy
@@ -3,6 +3,9 @@ description('This pipeline job is for Simplilearn Project 1')
     definition {
         cps {
 			sandbox()
+			GenericTrigger {
+				token("SimplilearnProject1PipelineJob")
+			}
             script(readFileFromWorkspace('pipeline/java_maven_build_with_docker_deployment.groovy'))
         }
     }
